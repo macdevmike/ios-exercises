4001ef0404ec344417ca6006ba68c1fd8a8f8e4c
Michael Henkelman <macjockey@me.com>
2015-04-22 18:13:05 -0700

    Commiting Equality Checkpoint

--- BlocExercises/BlocExercises/EqualityDeterminer.m
+++ BlocExercises/BlocExercises/EqualityDeterminer.m
@@ -11,18 +11,18 @@
 @implementation EqualityDeterminer
 
 - (BOOL) string:(NSString *)string1 isTheSameAsString:(NSString *)string2 {
-    /* WORK HERE */
-    return (string1 == string2);
+    BOOL cerialEquality = [string1 isEqual: string2];
+    return cerialEquality;
 }
 
 - (BOOL) number:(NSNumber *)number1 isTheSameAsNumber:(NSNumber *)number2 {
-    /* WORK HERE */
-    return (number1 == number2);
+    BOOL numberEquality = [number1 isEqualToNumber:number2];
+    return numberEquality;
 }
 
 - (BOOL) integer:(NSInteger)integer1 isGreaterThan:(NSInteger)integer2 {
-    /* WORK HERE */
-    return NO;
+    BOOL hiIsGreater = (integer1 > integer2);
+    return hiIsGreater;
 }
 
 @end
