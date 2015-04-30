5bec3512938ac9d9ec7eeb8ebb6bffb9e2def0c6
Michael Henkelman <macjockey@me.com>
2015-04-29 19:30:36 -0700

    Commiting Checkpoint 14

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -11,18 +11,29 @@
 @implementation NumberHandler
 
 - (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
-    /* WORK HERE */
-    return @0;
+    int num1 = [number intValue];
+    int num2 = num1 * 2;
+    NSNumber *newNum = [NSNumber numberWithInt:num2];
+    return newNum;
 }
 
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
-    /* WORK HERE */
-    return @[];
+    NSMutableArray *numArray = [NSMutableArray new];
+   
+    while (number <= otherNumber)
+    {
+        [numArray addObject:[NSNumber numberWithInteger:number]];
+        number++;
+
+    }
+    return numArray;
 }
 
 - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
-    /* WORK HERE */
-    return 0;
+    NSMutableArray *modArray = [arrayOfNumbers mutableCopy];
+    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:nil ascending:YES];
+    [modArray sortUsingDescriptors:@[sortDescriptor]];
+    return [modArray[0] integerValue];
 }
 
 @end
